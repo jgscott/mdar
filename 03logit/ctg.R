b1bb3a476e8b94c573c3c2265e8b494f41ab9e5f
James Scott <james.scott@mccombs.utexas.edu>
2013-05-21 08:18:57 -0500

    added fetal-heart-monitor example

---
+++ 03logit/ctg.R
@@ -0,0 +1,41 @@
+library(nnet)
+
+ctg = read.csv("ctg.csv", header=TRUE)
+summary(ctg)
+
+ml1 = multinom(NSP ~ (. - DR), data=ctg)
+
+# Step-down BIC
+mystep1 = step(ml1, scope = ~ (. - DR), k = log(nrow(ctg)))
+
+summary(mystep)
+
+
+#### Rescaling the columns of the design matrix
+
+# Construct a design matrix without the intercept column
+X = model.matrix(NSP ~ (. - DR - 1), data=ctg)
+
+# Compute the column means and column sd's
+colMeans(X)
+apply(X, 2, sd)
+
+# Center and scale the design matrix
+X = scale(X)
+colMeans(X)
+apply(X, 2, sd)
+
+# Reconstruct the data set
+ctgscale = data.frame(NSP = ctg$NSP, X)
+
+ml2 = multinom(NSP ~ ., data=ctgscale)
+
+# Step-down BIC
+mystep2 = step(ml2, scope=~., k = log(nrow(ctgscale)))
+summary(mystep2)
+
+# Make in-sample "predictions" and check the accuracy
+predict(mystep2)
+
+table(guess = predict(mystep2), truth = ctgscale$NSP)
+
