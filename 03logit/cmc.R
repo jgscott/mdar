3bf06d8086a813a45bcd6028f561dcac670e9bc2
James Scott <james.scott@mccombs.utexas.edu>
2013-05-21 08:55:52 -0500

    corrected cmc.R script

--- 03logit/cmc.R
+++ 03logit/cmc.R
@@ -1,10 +1,12 @@
 library(nnet)
 
+cmc = read.csv('cmc.csv', header=TRUE)
+
 ml1 = multinom(Method ~ Age + factor(Educ), data = cmc)
 summary(ml1)
 
 # Make "predictions" for class probabilities on original data
-pred1 = predict(ml1, contracept, type='probs')
+pred1 = predict(ml1, cmc, type='probs')
 
 
 # Put the actual outcomes and fitted class probabilities side by side
